7d149cf06a6d9636fb55ab5deb519a313ffd7a41
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-07-10 23:42:36 +0200

    init repo

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -1,21 +1,21 @@
-import style from "./ImageModal.module.css";
-import Modal from "react-modal";
-import { FcLike } from "react-icons/fc";
+import style from './ImageModal.module.css';
+import Modal from 'react-modal';
+import { FcLike } from 'react-icons/fc';
 
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
 const customStyles = {
   content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    background: "#232224",
-    border: "2px solid rgb(122 123 131)",
-    minWidth: "300px",
-    maxWidth: "800px",
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    background: '#232224',
+    border: '2px solid rgb(122 123 131)',
+    minWidth: '300px',
+    maxWidth: '800px',
   },
 };
 const ImageModal = ({ isOpen, likes, author, srcModal, closeModal }) => {
